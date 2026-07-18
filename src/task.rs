924a0045acb38f30d0ae2eced906d420ae655085
m-saif-1280 <msaifrehman10@proton.me>
2026-07-18 18:36:01 +0500

    feat(tasks): Add basic task struct

---
+++ src/task.rs
@@ -0,0 +1,26 @@
+pub struct Task {
+    done: bool,
+    title: String,
+}
+
+impl Task {
+    pub fn new(title: &str) -> Self {
+        Self {
+            done: false,
+            title: String::from(title),
+        }
+    }
+}
+
+impl Task {
+    pub fn done(&self) -> bool {
+        self.done
+    }
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub fn toggle_done(&mut self) {
+        self.done = !self.done
+    }
+}
