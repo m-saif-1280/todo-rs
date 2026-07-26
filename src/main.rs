db6dd7cfdda835f6e8b64d6fd643feef288b7985
m-saif-1280 <msaifrehman10@proton.me>
2026-07-26 11:57:27 +0500

    wip(task-serialization): Bubble I/O errors into main

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use todo_rs::App;
 
 fn main() -> std::io::Result<()> {
     let mut app = App::new();
+    if let Err(e) = app.load_tasks() {
+        ratatui::restore();
+        return Err(e);
+    }
 
     while app.is_running() {
         app.draw();
