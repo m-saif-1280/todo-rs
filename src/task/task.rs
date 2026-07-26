dfff868acb66747a4c810eaa28cf37eb6e704f1f
m-saif-1280 <msaifrehman10@proton.me>
2026-07-26 06:01:41 +0500

    refactor(task-serialization): Move task related things to their own module

---
+++ src/task/task.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Serialize, Deserialize)]
+pub struct Task {
+    done: bool,
+    title: String,
+}
+
+impl Task {
+    pub fn new(title: &str) -> Self {
+        Self {
+            done: false,
+            title: String::from(title),
+        }
+    }
+}
+
+impl Task {
+    pub fn done(&self) -> bool {
+        self.done
+    }
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub fn with_done(mut self, done: bool) -> Self {
+        self.done = done;
+        self
+    }
+    pub fn with_title(mut self, title: &str) -> Self {
+        self.title = String::from(title);
+        self
+    }
+
+    pub fn toggle_done(&mut self) {
+        self.done = !self.done
+    }
+}
